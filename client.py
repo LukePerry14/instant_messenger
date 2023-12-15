2e5761c38d6a91cfa73d2b3410df49a8e3afa14b
LukePerry14 <115703845+LukePerry14@users.noreply.github.com>
2023-12-15 20:38:40 +0000

    finished all code

--- client.py
+++ client.py
@@ -2,10 +2,34 @@ import select
 import socket
 import ipaddress
 import threading
-import shutil
 import sys
+import os
+import time
 
 global_stop = threading.Event()
+
+def hide_cursor():
+    sys.stdout.write("\033[?25l")
+    sys.stdout.flush()
+
+def show_cursor():
+    sys.stdout.write("\033[?25h")
+    sys.stdout.flush()
+
+def print_loading(loading_image):
+    hide_cursor()
+
+    anims = ["L", "Lo", "Loa", "Load", "Loadi", "Loadin", "Loading"]
+    pos = 0
+    while loading_image.is_set():
+        print("\r" + " " * 7 + "\r", end="", flush=True)
+        print(anims[pos], end="", flush=True)
+        pos += 1
+        pos = pos % 7
+        time.sleep(0.1)
+    print("\r" + " " * 7 + "\r", end="", flush=True)
+
+    show_cursor()
 
 def server_listen(client_socket):
     while not global_stop.is_set():
@@ -18,14 +42,40 @@ def server_listen(client_socket):
                     #if data is empty, the server closed the connection
                     global_stop.set()
                     break
+                elif "FILE_DOWNLOAD_HEADER" in data:
+                    parts = data.strip().split(" ")
+                    receive_file(client_socket, parts[1], parts[2])
+                elif data[0] == '[':
+                    box(string_to_array(data))
+                else:
+                    print(data)
 
-                characters, _ = shutil.get_terminal_size()
-                print('\r' + ' ' * characters + '\r' + data + "\n")
-                print("Enter a message or command ('help' for commands): ", end='', flush=True)
             else:
                 continue
         except ConnectionAbortedError:
             break
+
+def receive_file(client_socket, filename, save_directory):
+    loading_image = threading.Event()
+    loading_image.set()
+    load_thread = threading.Thread(target=print_loading, args=(loading_image,))
+    load_thread.start()
+    
+    try:
+        client_socket.send(b'ACK')
+        total_packets = int(client_socket.recv(1024).decode("utf-8"))
+        os.makedirs(save_directory, exist_ok=True)
+        with open(f"{save_directory}/{filename}", 'wb') as file:
+            for x in range(total_packets):
+                file_data = client_socket.recv(1024)
+                file.write(file_data)
+                client_socket.send(b'ACK')  # Send acknowledgment back to server
+        loading_image.clear()
+        load_thread.join()
+        
+        print(f"File '{filename}' received and saved to {save_directory}")
+    except Exception as e:
+        print(f"Error receiving file: {e}")
 
 def box(lines):
     longest = len(max(lines, key=len))
@@ -36,6 +86,14 @@ def box(lines):
 
 def help_box():
     box(["clients = see all other connected clients", "", "uni [name] = begin a 1-1 messenger with user [name]", "", "downlist = list all downloadable files from server", "", "down [file] [location] = download file [file] into folder [location]", "", "exit = disconnect from server and close program", "", "all other messages will be broadcast to all other clients connected",])
+
+def string_to_array(string):
+    string = string[1:-1]
+    string_arr = string.split(",")
+    for x in range(len(string_arr)):
+        string_arr[x] = string_arr[x].strip()[1:-1]
+    
+    return string_arr
 
 if len(sys.argv) != 4:
     print("Invalid number of arguments")
@@ -84,12 +142,24 @@ listen_thread.start()
 #start messaging
 while True:
     try:
-        message = input("Enter a message or command ('help' for commands): ")
+        message = input()
         if message.lower() == 'help':
             help_box()
+        elif " " in message:
+            parts = message.lower().split(" ")
+            if parts[0] == "uni":
+                client_socket.send(message.encode('utf-8'))
+                while True:
+                    message = input()
+                    client_socket.send(message.encode('utf-8'))
+                    if message.lower() == "exit":
+                        break
+            elif parts[0] == "down":
+                client_socket.send(message.encode('utf-8'))
         elif message.lower() == "exit":
             break
-        client_socket.send(message.encode('utf-8'))
+        else:
+            client_socket.send(message.encode('utf-8'))
     except EOFError:
         # Handle Ctrl+D (EOF) to exit gracefully
         print("\nExiting...")
--- server.py
+++ server.py
@@ -1,7 +1,30 @@
 import socket
 import threading
 import sys
+import os
+import logging
+import math
+#configure logging file
+logging.basicConfig(filename='server.log', filemode='w', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+downloads_directory = "./downloads"
+
+def send_file(clientName, client_socket, filename):
+    file_path = f"{downloads_directory}/{filename}"
+    file_size = os.path.getsize(file_path)
+    total_packets = math.ceil(file_size / 1024)
+    try:
+        client_socket.send(f"{total_packets}".encode("utf-8"))
+        with open(file_path, 'rb') as file:
+
+            for x in range(total_packets):
+                file_data = file.read(1024)
+                client_socket.send(file_data)
+                ack = client_socket.recv(1024)
+
+            logging.info(f"Sent file '{filename}' to {clientName}")
+    except FileNotFoundError:
+        logging.error(f"File '{filename}' not found on the server")
 
 def serverStart():
     if len(sys.argv) != 2:
@@ -19,7 +42,7 @@ def serverStart():
             exit()
 
     #intialise connections dictionary and port
-    host = '127.0.0.1'
+    host = "127.0.0.1"
     port = int(sys.argv[1])
 
     #create socket object skeleton
@@ -37,52 +60,93 @@ def serverStart():
 
 def broadcast(message, clientInfo, exclude=None):
     #send message to each client
-    for client_addr, client_data in clientInfo.items():
-        if exclude is None or client_addr not in exclude:
-            client_data["socket"].send(message.encode('utf-8'))
+    for clientName, client_data in clientInfo.items():
+        if exclude is None or clientName not in exclude:
+            client_data["socket"].send(message.encode("utf-8"))
 
 def Unicast(message, clientInfo, recipient):
-    clientInfo[recipient].send(message.encode('utf-8'))
+    clientInfo[recipient]["socket"].send(message.encode("utf-8"))
 
 def client_thread(client_socket, addr, clientInfo, names):
+    global downloads_directory
     print(f"Connection from {addr[0]}: {addr[1]}")
     
-    clientName = client_socket.recv(1024).decode('utf-8').strip()
+    clientName = client_socket.recv(1024).decode("utf-8").strip()
 
     if clientName in names:
-        client_socket.send("Client already exists with that name".encode('utf-8'))
+        client_socket.send("Client already exists with that name".encode("utf-8"))
+        logging.warning(f"refused connection from {addr[0]}: {addr[1]} - name {clientName} already exists")
+
         client_socket.close()
     else:
-        client_socket.send(f"Welcome to the Server {clientName}".encode('utf-8'))
+        logging.info(f"accepted connection from {clientName} at {addr[0]}: {addr[1]}")
+        client_socket.send(f"Welcome to the Server {clientName}".encode("utf-8"))
         names.append(clientName)
-        clientInfo[addr] = {
-            "name" : clientName,
+        clientInfo[clientName] = {
+            "address" : addr,
             "socket" : client_socket
         }
 
-        broadcast(f"{clientName} has joined", clientInfo, exclude=[addr])
+        broadcast(f"{clientName} has joined", clientInfo, exclude=[clientName])
+
+        logging.info(f"join message for {clientName} broadcast")
 
         while True:
             try:
-                message = client_socket.recv(1024)
+                message = client_socket.recv(1024).decode("utf-8")
                 if not message:
                     break
-                elif message == "clients":
-                    client_socket.send(f"Welcome to the Server {clientName}".encode('utf-8'))
-                elif message == "downlist":
-                    client_socket.send(f"Welcome to the Server {clientName}".encode('utf-8'))
-                elif message.split(" ")[0] == "uni":
-                    print("s")
-                elif message.split(" ")[0] == "down":
-                    print("s")
+                elif message.lower() == "clients":
+                    client_socket.send(str(names).encode("utf-8"))
+                    logging.info(f"sent connected client list to {clientName}")
+                elif message.lower() == "downlist":
+                    client_socket.send(str(os.listdir(downloads_directory)).encode("utf-8"))
+                    logging.info(f"sent downloadables list to {clientName}")
+                elif message.lower() == "help":
+                    continue
+                elif " " in message:
+                    brick = message.lower().split(" ")
+                    if brick[0] == "uni":
+                        recipient = brick[1]
+                        if (recipient in names) and (recipient != clientName):
+
+                            logging.info(f"Unicast started from {clientName} to {recipient}")
+                            client_socket.send(f"UniCasting to {recipient}".encode("utf-8"))
+                            while True:
+                                incoming_message = client_socket.recv(1024).decode("utf-8")
+                                if incoming_message.lower() == "exit":
+                                    client_socket.send(f"Leaving UniCast with {recipient}".encode("utf-8"))
+                                    logging.info(f"{clientName} exits unicast with {recipient}")
+                                    break
+                                else:
+                                    uni_message = f"{clientName} whispers: {incoming_message}"
+                                    Unicast(uni_message, clientInfo, recipient)
+                                    logging.info(f"{clientName} unicasts '{incoming_message}' to {recipient}")
+
+
+                        else:
+                            client_socket.send(f"{recipient} is not a valid client".encode("utf-8"))
+                            logging.warning(f"{clientName} attempts invalid unicast to {recipient}")
+                            continue
+
+                    elif brick[0] == "down":
+                        client_socket.send(f"FILE_DOWNLOAD_HEADER {brick[1]} {brick[2]}".encode("utf-8"))
+                        ack = client_socket.recv(1024)
+                        send_file(clientName, client_socket, brick[1])
+                    else:
+                        broadcast(f"{clientName}: {message}", clientInfo, exclude=[clientName])
+                        logging.info(f"{clientName} broadcasts '{message}'")
                 else:
-                    broadcast(f"{clientName}: {message}", clientInfo,)
+                    broadcast(f"{clientName}: {message}", clientInfo, exclude=[clientName])
+                    logging.info(f"{clientName} broadcasts '{message}'")
             except socket.error as e:
                 print(f"Error receiving data: {e}")
+                logging.error(f"error receiving data from {clientName} - server closed connection")
                 break
-
+        
+        logging.info(f"{clientName} disconnected from server")
         #delete client from dictionary and allowed names and broadcast leaving message
-        del clientInfo[addr]
+        del clientInfo[clientName]
         broadcast(f"{clientName} has left", clientInfo)
         names.remove(clientName)
 
@@ -103,6 +167,7 @@ def server_runtime():
             thread.start()
         except socket.error as e:
             print(f"Error accepting connection: {e}")
+            logging.error(f"Error accepting connection - {e}")
 
 
 server_runtime()
